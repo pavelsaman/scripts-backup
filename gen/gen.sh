491ca147406e884a9d388ec956325c0ca254d241
Pavel Saman <samanpavel@gmail.com>
2022-08-25 21:01:25 +0200

    fix: bug and style in gen.sh

--- gen/gen.sh
+++ gen/gen.sh
@@ -3,13 +3,13 @@
 char_set=()
 length=16
 
-function print_usage {
+print_usage() {
   cat <<END_USAGE
 Usage: gen [-c] [-d] [-s] [-l <lengh>] [-p] [-o] [-1] [-3] [-u] [-h]
 END_USAGE
 }
 
-function print_help {
+print_help() {
   print_usage
   cat <<END_HELP
 
@@ -29,27 +29,27 @@ END_HELP
 
 # xclip has to be installed so we can talk to clipboards
 # $DISPLAY has to be non-zero
-function check_requirements {
+check_requirements() {
 	local proceed=true
 	xclip -version >/dev/null 2>&1 || proceed=false
 	[[ -n "${DISPLAY}" ]] || proceed=false
 	[[ "${proceed}" = true ]] && return 0 || return 1
 }
 
-function add_chars {
+add_chars() {
 	char_set+=({a..z})
 	char_set+=({A..Z})
 }
 
-function add_digits {
+add_digits() {
 	char_set+=({0..9})
 }
 
-function add_schars {
+add_schars() {
 	char_set+=('+' '-' '?' ':' '@' '&' '#' '!' '*' '=' '(' ')' '[' ']' '{' '}' '%')
 }
 
-function gen_random {
+gen_random() {
 	local n
 	local pass
 	while (( length > ${#pass} )); do
@@ -61,7 +61,7 @@ function gen_random {
 
 # create a passphrase out of an English dictionary of words
 # it looks for a dictionary in this directory
-function create_passphrase {
+create_passphrase() {
   local dict="/usr/share/gen-dictionary/dictionary"
   local last_line
   last_line=$(/bin/wc -l "${dict}" | /bin/cut -d' ' -f1)
@@ -85,44 +85,43 @@ random_in_bucket=$((RANDOM % bucket_size))
   echo "${passphrase}"
 }
 
-########
-# main #
-########
+main() {
+  check_requirements || { echo "xclip not present or there is no DISPLAY variable" 1>&2; exit 1; }
+  
+  local print=false
+  local primary=false
+  local clipboard=false
+  local only_passphrase=false
 
-check_requirements || { echo "xclip not present or there is no DISPLAY variable" 1>&2; exit 1; }
-print=false
-primary=false
-clipboard=false
-only_passphrase=false
+  while getopts "l:pcdso13uh" opt; do
+    case "${opt}" in
+      l) length="${OPTARG}"   ;;
+      p) only_passphrase=true ;;
+      c) add_chars            ;;
+      d) add_digits           ;;
+      s) add_schars           ;;
+      o) print=true           ;;
+      1) primary=true         ;;
+      3) clipboard=true       ;;
+      u) print_usage; exit 0  ;;
+      h) print_help ; exit 0  ;;
+      *) print_help; exit 1   ;;
+    esac
+  done
 
-while getopts "l:pcdso13uh" opt; do
-  case "${opt}" in
-    l) length="${OPTARG}"   ;;
-    p) only_passphrase=true ;;
-    c) add_chars            ;;
-    d) add_digits           ;;
-    s) add_schars           ;;
-    o) print=true           ;;
-    1) primary=true         ;;
-    3) clipboard=true       ;;
-    u) print_usage; exit 0  ;;
-    h) print_help ; exit 0  ;;
-    *) print_help; exit 1   ;;
-  esac
-done
+  # some options are required
+  (( ${#char_set[@]} == 0 )) && [[ "${only_passphrase}" = false ]] && { print_usage >&2; exit 1; }
 
-# some options are required
-(( ${#char_set[@]} = 0 )) && [[ "${only_passphrase}" = false ]] && { print_usage 1>&2; exit 1; }
+  # get string
+  [[ "${only_passphrase}" = false ]] && result_string=$(gen_random) || result_string=$(create_passphrase)
 
-# get string
-[[ "${only_passphrase}" = false ]] && result_string=$(gen_random) || result_string=$(create_passphrase)
+  # print to the tty
+  [[ "${print}" = true ]] && echo "${result_string}"
+  # put into selections
+  [[ "${primary}" = true ]] && echo -n "${result_string}" | xclip -selection p
+  [[ "${clipboard}" = true ]] && echo -n "${result_string}" | xclip -selection c
+  # if neither -1 nor -3 are specified, place only into clipboard
+  [[ "${primary}" = false && "${clipboard}" = false ]] && echo -n "${result_string}" | xclip -selection c
+}
 
-# print to the tty
-[[ "${print}" = true ]] && echo "${result_string}"
-# put into selections
-[[ "${primary}" = true ]] && echo -n "${result_string}" | xclip -selection p
-[[ "${clipboard}" = true ]] && echo -n "${result_string}" | xclip -selection c
-# if neither -1 nor -3 are specified, place only into clipboard
-[[ "${primary}" = false && "${clipboard}" = false ]] && echo -n "${result_string}" | xclip -selection c
-
-exit 0
+main "${@}"

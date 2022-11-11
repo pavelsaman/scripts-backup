7e5061efcdc9c18c58eb60facc2dae285e9fc43f
Pavel Saman <psaman@cisco.com>
2022-11-11 21:59:58 +0100

    update git-move-tag script

--- git-move-tag/git-move-tag.sh
+++ git-move-tag/git-move-tag.sh
@@ -5,7 +5,10 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
-git tag -d "$1" \
+main_branch="$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
+git checkout "$main_branch" \
+  && git pull \
+  && git tag -d "$1" \
   && git push --delete origin "$1" \
   && git tag "$1" \
   && git push --tags
